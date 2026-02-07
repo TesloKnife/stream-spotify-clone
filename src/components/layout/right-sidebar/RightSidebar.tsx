4911c27db4d847d495f14d9e3576c0592c8867bc
teslo <teslo.knife@gmail.com>
2026-02-07 11:27:56 +0500

    .

--- src/components/layout/right-sidebar/RightSidebar.tsx
+++ src/components/layout/right-sidebar/RightSidebar.tsx
@@ -3,12 +3,22 @@
 export function RightSidebar() {
   return (
     <div className="bg-bg-secondary p-layout">
-      [ Verse 1] It might not be the right time I might not be the right one But
-      there's something about us I want to say Cause there's something between
-      us anyway [ Verse 2] I might not be the right one It might not be the
-      right time But there's something about us I've got to do Some kind of
-      secret I will share with you [ Refrain ] I need you more than anything in
-      my life I want you more than anything in my life
+      <div className="text-primary font-medium mb-5">[ Verse 1 ]</div>
+      <p>It might not be the right time</p>
+      <p>I might not be the right one</p>
+      <p>But there's something about us I want to say</p>
+      <p className="opacity-100">Cause there's something between us anyway</p>
+
+      <div className="text-primary font-medium mb-5">[ Verse 2 ]</div>
+      <p>I might not be the right one </p>
+      <p>It might not be the right time </p>
+      <p>But there's something about us</p>
+      <p>I've got to do </p>
+      <p>Some kind of secret I will share with you</p>
+
+      <div className="text-primary font-medium mb-5">[ Refrain ]</div>
+      <p>I need you more than anything in my life </p>
+      <p>I want you more than anything in my life</p>
     </div>
   );
 }
